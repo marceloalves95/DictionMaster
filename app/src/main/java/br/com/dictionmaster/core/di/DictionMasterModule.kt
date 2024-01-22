47fb90d04142dce6e07ceaaa204e158210ee89e3
Marcelo Alves <marceloalves1295@gmail.com>
2024-01-22 18:45:31 -0300

    Criação do módulo da injeção de dependência do projeto

--- app/src/main/java/br/com/dictionmaster/core/di/DictionMasterModule.kt
+++ app/src/main/java/br/com/dictionmaster/core/di/DictionMasterModule.kt
@@ -1,5 +1,13 @@
 package br.com.dictionmaster.core.di
 
+import br.com.dictionmaster.data.BASE_URL
+import br.com.dictionmaster.data.DictionMasterRepositoryImpl
+import br.com.dictionmaster.data.api.DictionMasterApi
+import br.com.dictionmaster.domain.repository.DictionMasterRepository
+import br.com.dictionmaster.domain.usecase.GetSearchWordUseCase
+import br.com.dictionmaster.network.service.Service
+import br.com.dictionmaster.presentation.ui.result.ResultViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -17,15 +25,26 @@ object DictionMasterModule {
     }
 
     private fun dataModule(): Module = module {
-
+        factory<DictionMasterApi> {
+            Service.createService(
+                baseUrl = BASE_URL
+            )
+        }
+        single<DictionMasterRepository> {
+            DictionMasterRepositoryImpl(api = get())
+        }
     }
 
     private fun domainModule(): Module = module {
-
+        factory {
+            GetSearchWordUseCase(repository = get())
+        }
     }
 
     private fun presentationModule(): Module = module {
-
+        viewModel {
+            ResultViewModel(getSearchWordUseCase = get())
+        }
     }
 
 }
