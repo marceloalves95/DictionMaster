5758ba738ef393a3018724ea85851dc8dc891d24
Marcelo Alves <marceloalves1295@gmail.com>
2024-01-24 23:34:25 -0300

    Ajuste das classes responsáveis pela chamada da api do projeto

--- network/src/main/java/br/com/dictionmaster/network/others/Service.kt
+++ network/src/main/java/br/com/dictionmaster/network/others/Service.kt
@@ -1,21 +1,31 @@
-package br.com.dictionmaster.network.service
+package br.com.dictionmaster.network.others
 
+import android.content.Context
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 
-object Service {
+class Service(
+    private val context: Context
+) {
+    private val cacheSize = 10L * 1024L * 1024L
+    private val cache = Cache(File(context.cacheDir, "http-cache"), cacheSize)
+
     fun createHttpClient(): OkHttpClient {
         val httpLoggingInterceptor = HttpLoggingInterceptor()
+
         httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         return OkHttpClient.Builder()
-            .addInterceptor(httpLoggingInterceptor)
-            .addNetworkInterceptor(httpLoggingInterceptor)
             .connectTimeout(1, TimeUnit.SECONDS)
             .readTimeout(40, TimeUnit.SECONDS)
             .writeTimeout(40, TimeUnit.SECONDS)
+            .addInterceptor(Interceptor(context))
+            .addNetworkInterceptor(httpLoggingInterceptor)
+            .cache(cache)
             .build()
     }
 
