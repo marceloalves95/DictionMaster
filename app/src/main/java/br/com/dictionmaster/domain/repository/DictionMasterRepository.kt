6b9fc8e5f52bcfb71be9136ef6028f43c1a98206
Marcelo Alves <marceloalves1295@gmail.com>
2024-01-26 20:21:27 -0300

    Ajustes no projeto

--- app/src/main/java/br/com/dictionmaster/domain/repository/DictionMasterRepository.kt
+++ app/src/main/java/br/com/dictionmaster/domain/repository/DictionMasterRepository.kt
@@ -1,7 +1,6 @@
 package br.com.dictionmaster.domain.repository
 
 import br.com.dictionmaster.domain.models.Search
-import retrofit2.http.Path
 
 interface DictionMasterRepository {
     suspend fun getSearchWord(word:String?): List<Search>
